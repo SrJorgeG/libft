ef55a8645d2fee9bcbfeaef2337e2cbeb9c8af5a
Jorge Gomez De Oliveira <jgomez-d@c2r7s6.42madrid.com>
2024-10-02 16:17:23 +0200

    created ft_strtrim

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jgomez-d <jgomez-d@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 01:03:52 by jgomez-d          #+#    #+#             */
-/*   Updated: 2024/10/02 00:56:07 by jgomez-d         ###   ########.fr       */
+/*   Updated: 2024/10/02 16:15:47 by jgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,4 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ft_memcpy(sd, s1, ft_strlen(s1));
 	ft_memcpy(sd + ft_strlen(s1), s2, ft_strlen(s2));
 	return (sd);
-}                        
+}
---
+++ ft_strtrim.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgomez-d <jgomez-d@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 13:22:47 by jgomez-d          #+#    #+#             */
+/*   Updated: 2024/10/02 16:15:24 by jgomez-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+	size_t	len;
+	char	*newstr;
+
+	if (!s1 || !set)
+		return (NULL);
+	i = 0;
+	len = ft_strlen(s1);
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	while (len > i && ft_strchr(set, s1[len - 1]))
+		len--;
+	newstr = ft_calloc(len - i + 1, sizeof(char));
+	if (!newstr)
+		return (NULL);
+	ft_memcpy(newstr, s1 + i, len - i);
+	return (newstr);
+}
