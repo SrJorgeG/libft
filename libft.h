9b86c6aeb32353325b0525cb2a14c4f2cc3e2d00
Jorge Gomez De Oliveira <jgomez-d@c2r12s6.42madrid.com>
2024-10-07 16:24:03 +0200

    Final push

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jgomez-d <jgomez-d@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 01:04:40 by jgomez-d          #+#    #+#             */
-/*   Updated: 2024/10/05 10:13:12 by jgomez-d         ###   ########.fr       */
+/*   Updated: 2024/10/07 16:04:42 by jgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,5 +66,5 @@ void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
